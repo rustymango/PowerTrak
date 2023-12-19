0dacc5d5a36458da1b4506fdd4928d2c0043f14a
Vincent Wong <vinny.wong@outlook.com>
2023-12-18 18:36:14 -0800

    Add project files.

---
+++ PowerTrak/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace PowerTrak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.getVideo = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(751, 347);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // getVideo
+            // 
+            this.getVideo.Location = new System.Drawing.Point(21, 369);
+            this.getVideo.Name = "getVideo";
+            this.getVideo.Size = new System.Drawing.Size(140, 69);
+            this.getVideo.TabIndex = 1;
+            this.getVideo.Text = "Get_Video";
+            this.getVideo.UseVisualStyleBackColor = true;
+            this.getVideo.Click += new System.EventHandler(this.getVideo_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(632, 369);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(140, 69);
+            this.cancel.TabIndex = 2;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.getVideo);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button getVideo;
+        private System.Windows.Forms.Button cancel;
+    }
+}
+
---
+++ PowerTrak/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace PowerTrak
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture = null;
+        IBackgroundSubtractor backgroundSubtractor;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void getVideo_Click(object sender, System.EventArgs e) {
+            try {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Video Files (*.mp4;)|*.mp4";
+
+                if (dialog.ShowDialog()==DialogResult.OK)
+                {
+                    capture = new VideoCapture(dialog.FileName);
+                    if (capture != null )
+                    {
+                        Mat frame = new Mat();
+                        capture.Read(frame);
+                        pictureBox1.Image = frame.ToBitmap();
+
+                        backgroundSubtractor = new BackgroundSubtractorMOG2();
+                        Application.Idle += Application_Idle;
+                    }
+                }
+            }
+
+            catch (Exception ex) { 
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Application_Idle(object sender, EventArgs e) {
+            try {
+                Mat frame = capture.QueryFrame();
+                if (frame.IsEmpty)
+                {
+                    Application.Idle -= Application_Idle;
+                    return;
+                }
+
+                Mat smoothFrame = new Mat();
+                CvInvoke.GaussianBlur(frame, smoothFrame, new Size(3, 3), 1);
+
+                Mat foregroundMask = new Mat();
+                backgroundSubtractor.Apply(smoothFrame, foregroundMask);
+
+                CvInvoke.Threshold(foregroundMask, foregroundMask, 200, 240, ThresholdType.Binary);
+                CvInvoke.MorphologyEx(foregroundMask, foregroundMask, Emgu.CV.CvEnum.MorphOp.Close,
+                    Mat.Ones(7,3,Emgu.CV.CvEnum.DepthType.Cv8U, 1), new Point(-1,-1), 1, BorderType.Reflect, new Emgu.CV.Structure.MCvScalar(0));
+
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                CvInvoke.FindContours(foregroundMask, contours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+                int minArea = 10000;
+                for (int i=0; i<contours.Size; i++)
+                {
+                    var bbox = CvInvoke.BoundingRectangle(contours[i]);
+                    var area = bbox.Width * bbox.Height;
+                    var ar = (float)bbox.Width / bbox.Height;
+
+                    if (area > minArea && ar<1.0) {
+                        CvInvoke.Rectangle(frame, bbox, new MCvScalar(0, 0, 255), 2);
+                    }
+                }
+
+                pictureBox1.Image = frame.ToBitmap();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void cancel_Click(object sender, System.EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
