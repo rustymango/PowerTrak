30ff0dcfc13035a99567d32903f40133c6accc94
Vincent Wong <vinny.wong@outlook.com>
2023-12-24 01:09:12 -0800

    Implemented hue mask for object identification and marking

--- PowerTrak/Form1.cs
+++ PowerTrak/Form1.cs
@@ -16,9 +16,8 @@ namespace PowerTrak
     public partial class Form1 : Form
     {
         VideoCapture capture = null;
-        IBackgroundSubtractor backgroundSubtractor;
-        int frameCount = 0;
-        System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        double realFPS;
+        double avgFrameDuration;
 
         public Form1()
         {
@@ -48,27 +47,17 @@ namespace PowerTrak
                         //// turns frame to array of pixel data (colour, etc.)
                         //pictureBox1.Image = frame.ToBitmap();
 
-                        // specs of background subtractor
-                        //backgroundSubtractor = new BackgroundSubtractorMOG2();
-
-                        //capture.ImageGrabbed += Application_Idle;
-                        //capture.Start();
-
-                        //myTimer.Interval = 1000 / 30;
-                        //myTimer.Tick -= new EventHandler(Application_Idle);
-                        //myTimer.Tick += new EventHandler(Application_Idle);
-                        //myTimer.Start();
-
+                        Stopwatch sw = new Stopwatch();
                         //int frameNumber = 0;
-                        //Stopwatch durSW = new Stopwatch();
-
                         //for (; ; )
                         //{
-                        //    durSW.Restart();
+                        //    sw.Restart();
                         //    capture.Read(frame);
+
+                        //    BoundingBoxes(frame);
                         //    pictureBox1.Image = frame.ToBitmap();
 
-                        //    double dur = durSW.ElapsedMilliseconds;
+                        //    double dur = sw.ElapsedMilliseconds;
                         //    Console.WriteLine($"avg time per frame {getFPS.calculateAvgDur(dur)} ms. fps {getFPS.calculateFPS()}. frameNo = {frameNumber++}");
 
 
@@ -81,15 +70,23 @@ namespace PowerTrak
                         //    }
                         //}
 
-                        Bitmap[] imageArray = GetVideoFrames();
-                        int i = 0;
+                        Mat[] imageArray = GetVideoFrames();
 
-                        foreach ( Bitmap image in imageArray )
+                        foreach (Mat mat in imageArray)
                         {
-                            pictureBox1.Image = imageArray[i];
-                            pictureBox1.Refresh();
-                            i++;
-                            Thread.Sleep(15);
+                            using (mat)
+                            {
+                                sw.Restart();
+                                //TrackBar.BoundingBoxes(mat);
+                                FindBar(mat.ToImage<Bgr, Byte>());
+
+                                double dur = sw.ElapsedMilliseconds;
+                                //pictureBox1.Image = mat.ToBitmap();
+                                pictureBox1.Refresh();
+
+                                Thread.Sleep(15);
+                                //if (dur.ElapsedMilliseconds < 16) Thread.Sleep(16 - (int)Math.Floor((double)dur.ElapsedMilliseconds));
+                            }
                         }
                     }
                 }
@@ -100,8 +97,83 @@ namespace PowerTrak
             }
         }
 
-        private void Application_Idle(object sender, EventArgs e) {
-            try {
+        internal void FindBar(Image<Bgr, byte> image)
+        {
+            // Convert the image to HSV
+            // "using" keyword disposes object after code block is run
+            using (Image<Hsv, byte> hsv = image.Convert<Hsv, byte>())
+            {
+                // Obtain the 3 channels (hue, saturation and value) that compose the HSV image
+                Image<Gray, byte>[] channels = hsv.Split();
+
+                try
+                {
+                    Image<Gray, byte> hueMask = channels[0];
+                    // Remove all pixels from the hue channel that are not in the range (currently yellow or red)
+                    CvInvoke.InRange(hueMask, new ScalarArray(new Gray(150).MCvScalar), new ScalarArray(new Gray(200).MCvScalar), hueMask);
+
+                    VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                    // grabs extreme outer contours by endpoints (compresses horizontal, vertical, and diagonal segments)
+                    CvInvoke.FindContours(hueMask, contours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+                    int minArea = 10000;
+
+                    for(int i=0; i<contours.Size; i++)
+                    {
+                        // returns rectangle for set of points
+                        var bbox = CvInvoke.BoundingRectangle(contours[i]);
+                        var area = bbox.Width * bbox.Height;
+                        var ar = (float)bbox.Width / bbox.Height;
+
+                        // modify condition
+                        if (area > minArea && ar < 1.0)
+                        {
+                            // Generates rectangle to the frame
+                            CvInvoke.Rectangle(hsv, bbox, new MCvScalar(0, 0, 255), 2);
+                        }
+                    }
+                    //Mat frame = new Mat();
+                    //CvInvoke.CvtColor(hueMask, frame, ColorConversion.Gray2Bgr);
+                    pictureBox1.Image = hsv.ToBitmap();
+                }
+                finally
+                {
+                    channels[1].Dispose();
+                    channels[2].Dispose();
+                }
+            }
+        }
+
+        private Mat[] GetVideoFrames()
+        {
+            int frames = (int)capture.Get(CapProp.FrameCount);
+            Mat[] imageArray = new Mat[frames];
+            Mat frame = new Mat();
+            int frameNumber = 0;
+            Stopwatch durSW = new Stopwatch();
+
+            for (int i = 0; i < frames; i++)
+            {
+                durSW.Restart();
+                if (frame == null)
+                {
+                    return imageArray;
+                }
+
+                capture.Read(frame);
+                imageArray[i] = frame.Clone();
+
+                realFPS = getFPS.calculateFPS();
+                avgFrameDuration = durSW.ElapsedMilliseconds;
+                Console.WriteLine($"avg time per frame {getFPS.calculateAvgDur(avgFrameDuration)} ms. fps {realFPS}. frameNo = {frameNumber++}");
+                if (durSW.ElapsedMilliseconds < 16) Thread.Sleep(16 - (int)Math.Floor((double)durSW.ElapsedMilliseconds));
+            } 
+            return imageArray;
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            try
+            {
                 // converts capture to matrix
                 Mat frame = capture.QueryFrame();
                 if (frame == null)
@@ -112,39 +184,12 @@ namespace PowerTrak
                 pictureBox1.Image = frame.ToBitmap();
                 double FPS = getFPS.calculateFPS();
                 Console.WriteLine(FPS);
-
-                //double sleepTime=0;
-                //if(FPS<)
-
-                //Thread.Sleep(1000 / (int)Math.Floor(FPS));
-                //Thread.Sleep(47);
-                //myTimer.Interval = 1000 / (int)Math.Floor(FPS);
             }
 
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-            frameCount += 1;
-            //Console.WriteLine(frameCount);
-        }
-
-        private Bitmap[] GetVideoFrames()
-        {
-            int frames = (int)capture.Get(CapProp.FrameCount);
-            Bitmap[] imageArray = new Bitmap[frames];
-            Mat frame = new Mat();
-
-            for (int i = 0; i < frames; i++)
-            {
-                capture.Read(frame);
-                if (frame == null)
-                {
-                    return imageArray;
-                }
-                imageArray[i] = frame.ToBitmap();
-            } 
-            return imageArray;
         }
 
         private void cancel_Click(object sender, System.EventArgs e)
@@ -152,42 +197,20 @@ namespace PowerTrak
             Application.Exit();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        internal void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
     }
 }
-//Mat smoothFrame = new Mat();
-//// reduces image noise, so smaller movements not detected, size is tolerance of what gets muted
-//CvInvoke.GaussianBlur(frame, smoothFrame, new Size(3, 3), 1);
 
-//// removes non-moving background from image and transfers result to "foreground mask"
-//Mat foregroundMask = new Mat();
-//backgroundSubtractor.Apply(smoothFrame, foregroundMask);
+//capture.ImageGrabbed += Application_Idle;
+//capture.Start();
 
-//// filters out pixels of certain specs from foreground mask
-//CvInvoke.Threshold(foregroundMask, foregroundMask, 200, 240, ThresholdType.Binary);
+//myTimer.Interval = 1000 / 30;
+//myTimer.Tick -= new EventHandler(Application_Idle);
+//myTimer.Tick += new EventHandler(Application_Idle);
+//myTimer.Start();
 
-//// MorphOp.Close fills in holes (dilate-->erode/shrink), Mat.Ones = array of "1" values where location/type is satisfied,
-//// also reflects upside down?
-//CvInvoke.MorphologyEx(foregroundMask, foregroundMask, MorphOp.Close, Mat.Ones(7,3,DepthType.Cv8U,1), 
-//    new Point(-1,-1), 1, BorderType.Reflect, new MCvScalar(0));
-
-//VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
-//// grabs extreme outer contours by endpoints (compresses horizontal, vertical, and diagonal segments)
-//CvInvoke.FindContours(foregroundMask, contours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
-//int minArea=100000;
-//for (int i=0; i<contours.Size; i++)
-//{
-//    // returns rectangle for set of points
-//    var bbox = CvInvoke.BoundingRectangle(contours[i]);
-//    var area = bbox.Width * bbox.Height;
-//    var ar = (float)bbox.Width / bbox.Height;
-
-//    // modify condition
-//    if (area > minArea && ar<1.0) {
-//        // Generates rectangle to the frame
-//        CvInvoke.Rectangle(frame, bbox, new MCvScalar(0, 0, 255), 2);
-//    }
-//}
+//int frameNumber = 0;
+//Stopwatch durSW = new Stopwatch();
