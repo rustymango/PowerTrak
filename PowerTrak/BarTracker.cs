59a0c7d63b62a3643ab577316b723c2e8455232c
Vincent Wong <vinny.wong@outlook.com>
2023-12-25 02:25:45 -0800

    Prototyped bar direction tracking and timing

---
+++ PowerTrak/BarTracker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace PowerTrak
+{
+    internal class BarTracker
+    {
+        public double tempoTime;
+        private Stopwatch tempoSW = new Stopwatch();
+        public int pauseY=0;
+        public double pauseTime;
+        private Stopwatch pauseSW = new Stopwatch();
+
+        internal bool UnrackStatus()
+        {
+            return true;
+        }
+
+        internal int TrackDownwards(int prevY, Rectangle bbox, int tolerance)
+        {
+            if(!tempoSW.IsRunning) tempoSW.Restart();
+
+            if (bbox.Y - prevY < tolerance) 
+            {
+                pauseY = bbox.Y;
+                tempoTime = tempoSW.ElapsedMilliseconds;
+
+                tempoSW.Stop();
+                if (!pauseSW.IsRunning) pauseSW.Restart();
+                Console.WriteLine("TempoSW stopped.");
+            }
+
+            return bbox.Y;
+        }
+
+        // incorporate velocity tracking later
+        internal int TrackUpwards(int pauseY, Rectangle bbox)
+        {
+            pauseTime = pauseSW.ElapsedMilliseconds;
+            pauseSW.Stop();
+            return 0;
+        }
+    }
+}
