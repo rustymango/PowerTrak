1e39c9cac0de6b11f41938038a0864254674b333
Vincent Wong <vinny.wong@outlook.com>
2024-01-01 01:57:14 -0800

    Added feature to handle multiple reps in a single video

--- PowerTrak/BarTracker.cs
+++ PowerTrak/BarTracker.cs
@@ -1,39 +1,50 @@
 ﻿using System;
 using System.Drawing;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace PowerTrak
 {
     internal class BarTracker
     {
-        public double tempoTime;
-        private Stopwatch tempoSW = new Stopwatch();
-        public int pauseY=0;
-        public double pauseTime;
-        private Stopwatch pauseSW = new Stopwatch();
+        public int pauseY = 0;
 
+        double tempoTime;
+        public List<double> tempoTimes = new List<double>();
+        Stopwatch tempoSW = new Stopwatch();
+
+        double pauseTime;
+        public List<double> pauseTimes = new List<double>();
+        Stopwatch pauseSW = new Stopwatch();
+
+        int currRep = 1;
+        int nextRep = 1;
+
+        // WIP
         internal bool UnrackStatus()
         {
             return true;
         }
 
-        internal int TrackDownwards(int prevY, Rectangle bbox, int tolerance)
+        internal int TrackDownwards(int prevY, Rectangle bbox, int topY, int tolerance)
         {
-            if(!tempoSW.IsRunning) tempoSW.Restart();
+            if (!tempoSW.IsRunning && bbox.Y - topY > 10) tempoSW.Restart();
 
-            if (bbox.Y - prevY == 0) 
+            if (bbox.Y - prevY == 0)
             {
+                currRep = nextRep;
                 pauseY = bbox.Y;
                 if (pauseSW.IsRunning) pauseTime += pauseSW.ElapsedMilliseconds;
+                Console.WriteLine($"Pause Time: {pauseTime}");
                 pauseSW.Restart();
             }
-            pauseTime = 0;
+            else { pauseTime = 0; Console.WriteLine("Pause timer reset"); }
 
             return bbox.Y;
         }
 
         // incorporate velocity tracking later
-        internal void TrackUpwards(int pauseY, Rectangle bbox)
+        internal int TrackUpwards(Rectangle bbox)
         {
             if (tempoSW.IsRunning && pauseSW.IsRunning)
             {
@@ -43,7 +54,17 @@ namespace PowerTrak
                 tempoTime = tempoTime - pauseTime;
                 Console.WriteLine("TempoSW stopped.");
             }
-            return;
+
+            if (currRep == nextRep) 
+            {
+                Console.WriteLine($"Final Pause Time: {pauseTime}");
+                pauseTimes.Add(pauseTime);
+                tempoTimes.Add(tempoTime);
+                tempoTime = 0; pauseTime = 0;
+
+                nextRep += 1;
+            }
+            return bbox.Y;
         }
     }
 }
